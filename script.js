2d13967384597cb7e26cf36c52de1f70ca1bd725
BIBIL <99636054+spacebin-org@users.noreply.github.com>
2022-10-14 22:40:53 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-  function sendMessage() {
-  function sleep(milliseconds) {
-  const date = Date.now();
-  let currentDate = null;
   do {
     currentDate = Date.now();
   } while (currentDate - date < milliseconds);
